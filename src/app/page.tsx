c7a91b84210dc99161ef5cc2ffbf5330eee4cad5
askhan963 <awaisknas963@gmail.com>
2024-12-10 16:54:03 +0500

    Added ContactMe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Experience from "@/app/components/Experience";
 import MyCVs from "@/app/components/MyCVs";
 import Contact from "@/app/components/Contact";
 import Footer from "./components/Footer";
+import ContactMe from "./components/ContactMe";
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
       <MyCVs />
       <Contact />
       <Footer />
+      <ContactMe /> 
     </div>
   );
 }
