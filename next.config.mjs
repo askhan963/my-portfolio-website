6706598c3ea3261bab873a5da72960923507f83d
askhan963 <awaisknas963@gmail.com>
2025-05-31 01:38:05 +0500

    Add: Optimization rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.mjs
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    return config;
+  },
+  experimental: {
+    optimizeFonts: true,
+  },
+};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const { white } = require('tailwindcss/colors');
 module.exports = {
-  darkMode: 'class', 
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,31 +10,31 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-       white: '#fefefe',
+        white: "#fefefe",
       },
       fontFamily: {
-        sans: ['Poppins', 'Inter', 'Space Grotesk', 'sans-serif'],
-        heading: ['Poppins', 'Space Grotesk', 'sans-serif'],
-        body: ['Inter', 'sans-serif'],
-        display: ['Space Grotesk', 'sans-serif']
+        sans: ["Poppins", "Inter", "Space Grotesk", "sans-serif"],
+        heading: ["Poppins", "Space Grotesk", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+        display: ["Space Grotesk", "sans-serif"],
       },
       fontSize: {
-        xs: ['0.75rem', { lineHeight: '1.5' }],
-        sm: ['0.875rem', { lineHeight: '1.5715' }],
-        base: ['1rem', { lineHeight: '1.5' }],
-        lg: ['1.125rem', { lineHeight: '1.5' }],
-        xl: ['1.25rem', { lineHeight: '1.625' }],
-        '2xl': ['1.5rem', { lineHeight: '2' }],
-        '3xl': ['2rem', { lineHeight: '2.25' }],
-        '4xl': ['2.5rem', { lineHeight: '2.5' }],
-        '5xl': ['3rem', { lineHeight: '1' }],
-        '6xl': ['3.75rem', { lineHeight: '1' }],
-        '7xl': ['4.5rem', { lineHeight: '1' }],
-        '8xl': ['6rem', { lineHeight: '1' }],
-        '9xl': ['8rem', { lineHeight: '1' }],
-      }
+        xs: ["0.75rem", { lineHeight: "1.5" }],
+        sm: ["0.875rem", { lineHeight: "1.5715" }],
+        base: ["1rem", { lineHeight: "1.5" }],
+        lg: ["1.125rem", { lineHeight: "1.5" }],
+        xl: ["1.25rem", { lineHeight: "1.625" }],
+        "2xl": ["1.5rem", { lineHeight: "2" }],
+        "3xl": ["2rem", { lineHeight: "2.25" }],
+        "4xl": ["2.5rem", { lineHeight: "2.5" }],
+        "5xl": ["3rem", { lineHeight: "1" }],
+        "6xl": ["3.75rem", { lineHeight: "1" }],
+        "7xl": ["4.5rem", { lineHeight: "1" }],
+        "8xl": ["6rem", { lineHeight: "1" }],
+        "9xl": ["8rem", { lineHeight: "1" }],
+      },
     },
   },
-  
+
   plugins: [],
-}
+};
