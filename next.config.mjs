849203c6ee1665d1807bc534cbede3fc9b5e71f4
askhan963 <awaisknas963@gmail.com>
2025-09-11 21:52:49 +0500

    Update : packages

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
     return config;
